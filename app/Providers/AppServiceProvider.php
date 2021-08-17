b19ae0793e3a653900cec478039c2ef695417da2
Gabriel Neves <ti1.dg@udv.org.br>
2021-08-17 13:16:39 -0300

    [add] Map abstract Contract to concret  Repository

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,6 +13,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+
+        $models = ['Customer', 'Subscription', 'Pet'];
+
+        foreach ($models as $model) {
+            $this->app->bind("App\Repositories\Contracts\\{$model}RepositoryInterface", "App\Repositories\\{$model}RepositoryEloquent");
+        }
+
     }
 }
